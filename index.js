cb06a6c6c99605265144e9cb3bd0bfcc2d1bddcf
cereceres <jesus.edelcereceres@gmail.com>
2018-06-01 16:36:43 -0600

    test to return if times is zero are fixed

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
 const validating = require('./lib/cb-with-schema');
+const withoutSchema = require('./lib/cb-without-schema');
 const defaultHandlerError = (error) => {
-    throw error
-}
+    throw error;
+};
 module.exports = function(cb, onlySomeTimes = 1, schema, inBadParams) {
-    const self = this;
     if (typeof cb !== 'function') throw new Error('Only can wrap functions');
-    if (typeof inBadParams !== 'function') inBadParams = defaultHandlerError
-    if (onlySomeTimes === 0) onlySomeTimes = Infinity;
-    if (schema) return validating({cb, onlySomeTimes, schema});
-
-    const cbWrapped = (...args) => {
-        if (cbWrapped.cbIsCalled >= onlySomeTimes) return;
-
-        return cb.call(self, ...args, cbWrapped.cbIsCalled++);
-    };
-    cbWrapped.cbIsCalled = 0;
-    cbWrapped.reset = () => {
-        cbWrapped.cbIsCalled = 0;
-        return cbWrapped;
-    };
-    return cbWrapped;
+    if (typeof inBadParams !== 'function') inBadParams = defaultHandlerError;
+    if (onlySomeTimes === 0 && !schema) return cb;
+    if (schema) return validating.call(this, { cb, onlySomeTimes, schema });
+    return withoutSchema.call(this, { cb, onlySomeTimes });
 };
